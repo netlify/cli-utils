c77a5e8142ef482c862c9fec86061977b05a4624
Bret Comnes <bcomnes@gmail.com>
2018-12-20 17:34:00 -0800

    Stub out failing tests

---
+++ src/cli-state/index.test.js
@@ -0,0 +1,40 @@
+const test = require('ava')
+const findRoot = require('.')
+const path = require('path')
+const tempy = require('tempy')
+
+test('Can initialize', t => {
+  t.fail('Not implemented')
+})
+
+test('All getter', t => {
+  t.fail('Not implemented')
+})
+
+test('All Setter', t => {
+  t.fail('Not implemented')
+})
+
+test('Size Getter', t => {
+  t.fail('Not implemented')
+})
+
+test('get', t => {
+  t.fail('Not implemented')
+})
+
+test('set', t => {
+  t.fail('Not implemented')
+})
+
+test('has', t => {
+  t.fail('Not implemented')
+})
+
+test('delete', t => {
+  t.fail('Not implemented')
+})
+
+test('clear', t => {
+  t.fail('Not implemented')
+})
